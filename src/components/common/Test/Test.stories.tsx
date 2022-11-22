dafd02d93703abf53d3dc1f9f316a1d1796bbe5e
Go Gwangseo <rufflain918@gmail.com>
2022-11-22 10:41:09 +0900

    chore: add test component

---
+++ src/components/common/Test/Test.stories.tsx
@@ -0,0 +1,8 @@
+import Test from './Test';
+
+export default {
+  title: 'common/Test',
+  component: Test,
+};
+
+export const Default = Test;
---
+++ src/components/common/Test/Test.tsx
@@ -0,0 +1,7 @@
+interface Props {
+  text?: string;
+}
+
+const Test = ({ text }: Props) => <p>{text}</p>;
+
+export default Test;
