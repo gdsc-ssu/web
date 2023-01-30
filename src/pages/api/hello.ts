9c9b1e471c8c71884a2ad76e4e38447c98e625d7
Go Gwangseo <rufflain918@gmail.com>
2023-01-30 22:40:31 +0900

    fix: remove runtime option

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,6 @@
 import { prisma } from '@/lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export const config = {
-  runtime: 'nodejs',
-};
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
