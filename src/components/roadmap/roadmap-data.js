c688215b6c82d70bf0babcf1555092e9022a582d
kweonsikyung <ekrk6632@naver.com>
2023-02-02 03:39:15 +0900

    add: roadmap data

---
+++ src/components/roadmap/roadmap-data.js
@@ -0,0 +1,176 @@
+const services_data = [
+  // Web Frontend data
+  {
+    id: 1,
+    title: 'HTML',
+    text_1:
+      '기본지식을 습득하고, 형태와 유효성 검사를 공부해야합니다. \n   이후 규약 및 최션의 관례에 대해 공부하면 되겠습니다.',
+    status: 'active',
+  },
+  {
+    id: 2,
+    title: 'Hype Boy',
+    text_1:
+      "예민하대 나 lately 너 없이는 매일 매일이 yeah 재미없어 어쩌지 I just want you Call my phone right now I just wanna hear you're mine ‘Cause I know what you like boy You're my chemical hype boy 내 지난날들은 눈 뜨면 잊는 꿈 Hype boy 너만 원해 Hype boy 내가 전해 And we can go high 말해봐 yeah 느껴봐 mm mm Take him to the sky You know I hype you boy",
+    status: 'active',
+  },
+  {
+    id: 3,
+    title: 'Love Dive',
+    text_1:
+      "네가 참 궁금해 그건 너도 마찬가지 이거면 충분해 쫓고 쫓는 이런 놀이 참을 수 없는 이끌림과 호기심 묘한 너와 나 두고 보면 알겠지 Woo 눈동자 아래로 Woo 감추고 있는 거 Woo yeah It's so bad It's good 난 그 맘을 좀 봐야겠어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive Woo lalalalalalala Woo 어서 와서 love dive Woo oh perfect sacrifice yeah 숨 참고 love dive 마음은 이렇게 알다가도 모르지 사랑이라는 건 한순간에 필 테니 직접 들어와 두 눈으로 확인해 내 맘 가장 깊은 데로 오면 돼 Woo 망설일 시간은 Woo 3초면 되는 걸 Woo yeah It's so bad It's good 원하면 감히 뛰어들어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive",
+    status: 'active',
+  },
+  {
+    id: 4,
+    title: 'I love GDSC❤️',
+    text_1:
+      'I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️',
+    status: 'active',
+  },
+  // Git data
+  {
+    id: 5,
+    title: 'Git',
+    text_1:
+      '기본지식을 습득하고, 형태와 유효성 검사를 공부해야합니다. \n   이후 규약 및 최션의 관례에 대해 공부하면 되겠습니다.',
+    status: 'false',
+  },
+  {
+    id: 6,
+    title: 'Hype Boy',
+    text_1:
+      "예민하대 나 lately 너 없이는 매일 매일이 yeah 재미없어 어쩌지 I just want you Call my phone right now I just wanna hear you're mine ‘Cause I know what you like boy You're my chemical hype boy 내 지난날들은 눈 뜨면 잊는 꿈 Hype boy 너만 원해 Hype boy 내가 전해 And we can go high 말해봐 yeah 느껴봐 mm mm Take him to the sky You know I hype you boy",
+    status: 'false',
+  },
+  {
+    id: 7,
+    title: 'Love Dive',
+    text_1:
+      "네가 참 궁금해 그건 너도 마찬가지 이거면 충분해 쫓고 쫓는 이런 놀이 참을 수 없는 이끌림과 호기심 묘한 너와 나 두고 보면 알겠지 Woo 눈동자 아래로 Woo 감추고 있는 거 Woo yeah It's so bad It's good 난 그 맘을 좀 봐야겠어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive Woo lalalalalalala Woo 어서 와서 love dive Woo oh perfect sacrifice yeah 숨 참고 love dive 마음은 이렇게 알다가도 모르지 사랑이라는 건 한순간에 필 테니 직접 들어와 두 눈으로 확인해 내 맘 가장 깊은 데로 오면 돼 Woo 망설일 시간은 Woo 3초면 되는 걸 Woo yeah It's so bad It's good 원하면 감히 뛰어들어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive",
+    status: 'false',
+  },
+  {
+    id: 8,
+    title: 'I love GDSC❤️',
+    text_1:
+      'I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️',
+    status: 'false',
+  },
+  // Cross Platform Data
+  {
+    id: 9,
+    title: 'Cross Platform',
+    text_1:
+      '기본지식을 습득하고, 형태와 유효성 검사를 공부해야합니다. \n   이후 규약 및 최션의 관례에 대해 공부하면 되겠습니다.',
+    status: 'false',
+  },
+  {
+    id: 10,
+    title: 'Hype Boy',
+    text_1:
+      "예민하대 나 lately 너 없이는 매일 매일이 yeah 재미없어 어쩌지 I just want you Call my phone right now I just wanna hear you're mine ‘Cause I know what you like boy You're my chemical hype boy 내 지난날들은 눈 뜨면 잊는 꿈 Hype boy 너만 원해 Hype boy 내가 전해 And we can go high 말해봐 yeah 느껴봐 mm mm Take him to the sky You know I hype you boy",
+    status: 'false',
+  },
+  {
+    id: 11,
+    title: 'Love Dive',
+    text_1:
+      "네가 참 궁금해 그건 너도 마찬가지 이거면 충분해 쫓고 쫓는 이런 놀이 참을 수 없는 이끌림과 호기심 묘한 너와 나 두고 보면 알겠지 Woo 눈동자 아래로 Woo 감추고 있는 거 Woo yeah It's so bad It's good 난 그 맘을 좀 봐야겠어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive Woo lalalalalalala Woo 어서 와서 love dive Woo oh perfect sacrifice yeah 숨 참고 love dive 마음은 이렇게 알다가도 모르지 사랑이라는 건 한순간에 필 테니 직접 들어와 두 눈으로 확인해 내 맘 가장 깊은 데로 오면 돼 Woo 망설일 시간은 Woo 3초면 되는 걸 Woo yeah It's so bad It's good 원하면 감히 뛰어들어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive",
+    status: 'false',
+  },
+  {
+    id: 12,
+    title: 'I love GDSC❤️',
+    text_1:
+      'I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️',
+    status: 'false',
+  },
+  // Server/Cloud Data
+  {
+    id: 13,
+    title: 'Git',
+    text_1:
+      '기본지식을 습득하고, 형태와 유효성 검사를 공부해야합니다. \n   이후 규약 및 최션의 관례에 대해 공부하면 되겠습니다.',
+    status: 'false',
+  },
+  {
+    id: 14,
+    title: 'Hype Boy',
+    text_1:
+      "예민하대 나 lately 너 없이는 매일 매일이 yeah 재미없어 어쩌지 I just want you Call my phone right now I just wanna hear you're mine ‘Cause I know what you like boy You're my chemical hype boy 내 지난날들은 눈 뜨면 잊는 꿈 Hype boy 너만 원해 Hype boy 내가 전해 And we can go high 말해봐 yeah 느껴봐 mm mm Take him to the sky You know I hype you boy",
+    status: 'false',
+  },
+  {
+    id: 15,
+    title: 'Love Dive',
+    text_1:
+      "네가 참 궁금해 그건 너도 마찬가지 이거면 충분해 쫓고 쫓는 이런 놀이 참을 수 없는 이끌림과 호기심 묘한 너와 나 두고 보면 알겠지 Woo 눈동자 아래로 Woo 감추고 있는 거 Woo yeah It's so bad It's good 난 그 맘을 좀 봐야겠어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive Woo lalalalalalala Woo 어서 와서 love dive Woo oh perfect sacrifice yeah 숨 참고 love dive 마음은 이렇게 알다가도 모르지 사랑이라는 건 한순간에 필 테니 직접 들어와 두 눈으로 확인해 내 맘 가장 깊은 데로 오면 돼 Woo 망설일 시간은 Woo 3초면 되는 걸 Woo yeah It's so bad It's good 원하면 감히 뛰어들어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive",
+    status: 'false',
+  },
+  {
+    id: 16,
+    title: 'I love GDSC❤️',
+    text_1:
+      'I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️',
+    status: 'false',
+  },
+  // AI/ML Data
+  {
+    id: 17,
+    title: 'Git',
+    text_1:
+      '기본지식을 습득하고, 형태와 유효성 검사를 공부해야합니다. \n   이후 규약 및 최션의 관례에 대해 공부하면 되겠습니다.',
+    status: 'false',
+  },
+  {
+    id: 18,
+    title: 'Hype Boy',
+    text_1:
+      "예민하대 나 lately 너 없이는 매일 매일이 yeah 재미없어 어쩌지 I just want you Call my phone right now I just wanna hear you're mine ‘Cause I know what you like boy You're my chemical hype boy 내 지난날들은 눈 뜨면 잊는 꿈 Hype boy 너만 원해 Hype boy 내가 전해 And we can go high 말해봐 yeah 느껴봐 mm mm Take him to the sky You know I hype you boy",
+    status: 'false',
+  },
+  {
+    id: 19,
+    title: 'Love Dive',
+    text_1:
+      "네가 참 궁금해 그건 너도 마찬가지 이거면 충분해 쫓고 쫓는 이런 놀이 참을 수 없는 이끌림과 호기심 묘한 너와 나 두고 보면 알겠지 Woo 눈동자 아래로 Woo 감추고 있는 거 Woo yeah It's so bad It's good 난 그 맘을 좀 봐야겠어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive Woo lalalalalalala Woo 어서 와서 love dive Woo oh perfect sacrifice yeah 숨 참고 love dive 마음은 이렇게 알다가도 모르지 사랑이라는 건 한순간에 필 테니 직접 들어와 두 눈으로 확인해 내 맘 가장 깊은 데로 오면 돼 Woo 망설일 시간은 Woo 3초면 되는 걸 Woo yeah It's so bad It's good 원하면 감히 뛰어들어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive",
+    status: 'false',
+  },
+  {
+    id: 20,
+    title: 'I love GDSC❤️',
+    text_1:
+      'I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️',
+    status: 'false',
+  },
+  // DevOps Data
+  {
+    id: 21,
+    title: 'Git',
+    text_1:
+      '기본지식을 습득하고, 형태와 유효성 검사를 공부해야합니다. \n   이후 규약 및 최션의 관례에 대해 공부하면 되겠습니다.',
+    status: 'false',
+  },
+  {
+    id: 22,
+    title: 'Hype Boy',
+    text_1:
+      "예민하대 나 lately 너 없이는 매일 매일이 yeah 재미없어 어쩌지 I just want you Call my phone right now I just wanna hear you're mine ‘Cause I know what you like boy You're my chemical hype boy 내 지난날들은 눈 뜨면 잊는 꿈 Hype boy 너만 원해 Hype boy 내가 전해 And we can go high 말해봐 yeah 느껴봐 mm mm Take him to the sky You know I hype you boy",
+    status: 'false',
+  },
+  {
+    id: 23,
+    title: 'Love Dive',
+    text_1:
+      "네가 참 궁금해 그건 너도 마찬가지 이거면 충분해 쫓고 쫓는 이런 놀이 참을 수 없는 이끌림과 호기심 묘한 너와 나 두고 보면 알겠지 Woo 눈동자 아래로 Woo 감추고 있는 거 Woo yeah It's so bad It's good 난 그 맘을 좀 봐야겠어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive Woo lalalalalalala Woo 어서 와서 love dive Woo oh perfect sacrifice yeah 숨 참고 love dive 마음은 이렇게 알다가도 모르지 사랑이라는 건 한순간에 필 테니 직접 들어와 두 눈으로 확인해 내 맘 가장 깊은 데로 오면 돼 Woo 망설일 시간은 Woo 3초면 되는 걸 Woo yeah It's so bad It's good 원하면 감히 뛰어들어 Narcissistic my god I love it 서로를 비춘 밤 아름다운 까만 눈빛 더 빠져 깊이 (넌 내게로 난 네게로) 숨 참고 love dive",
+    status: 'false',
+  },
+  {
+    id: 24,
+    title: 'I love GDSC❤️',
+    text_1:
+      'I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️I love GDSC❤️',
+    status: 'false',
+  },
+];
