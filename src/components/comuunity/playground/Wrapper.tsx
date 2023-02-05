9029cd167575a2971b73d40edf47db5cb2058a0d
Jun99uu <igun0423@naver.com>
2023-02-05 17:52:38 +0900

    fix: fix Page Wrapper

--- src/components/comuunity/playground/Wrapper.tsx
+++ src/components/comuunity/playground/Wrapper.tsx
@@ -1,7 +1,15 @@
+interface Props {
+  children: React.ReactNode;
+  topColor: string;
+  bottomColor: string;
+}
+
 /**
  * 모바일 중심형 페이지 Wrapper Component
+ * @param topColor 왼쪽 상단 색상 헥사코드(eg. #00A4CA)
+ * @param bottomColor 우측 하단 색상 헥사코드
  */
-const Wrapper = (props: { children: React.ReactNode }) => {
+const Wrapper = (props: Props) => {
   return (
     <div className="container">
       {props.children}
@@ -9,13 +17,21 @@ const Wrapper = (props: { children: React.ReactNode }) => {
         .container {
           width: 31.2vw;
           min-height: 100vh;
-          background-color: white;
+          background: linear-gradient(
+            135deg,
+            ${props.topColor},
+            ${props.bottomColor}
+          );
+        }
 
-          @media screen and (min-width: 500px) {
+        @media screen and (min-width: 500px) {
+          .container {
             min-width: 500px;
           }
+        }
 
-          @media screen and (max-width: 500px) {
+        @media screen and (max-width: 500px) {
+          .container {
             width: 100vw;
           }
         }
