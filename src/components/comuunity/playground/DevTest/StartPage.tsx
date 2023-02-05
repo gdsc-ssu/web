db6204553590b2a06d9f46cbeddc829841a99038
Jun99uu <igun0423@naver.com>
2023-02-05 18:42:57 +0900

    feat: add devtest inner content changing

--- src/components/comuunity/playground/DevTest/StartPage.tsx
+++ src/components/comuunity/playground/DevTest/StartPage.tsx
@@ -1,9 +1,20 @@
-import { StartButton, Button, TitleBox, VisitorBox } from '../common';
+import { StartButton, TitleBox, VisitorBox } from '../common';
+import {
+  COLORS,
+  TEXT_STYLE_BODY_PC,
+  TEXT_STYLE_BODY_MOBILE,
+} from '../common/token';
 import { devTestLogo } from '@/resources/communityRes';
+import { Dispatch, SetStateAction } from 'react';
+
+interface Props {
+  setStage: Dispatch<SetStateAction<number>>;
+}
+
 /**
  * 시작하기 버튼이 있는 테스트 시작 페이지
  */
-const StartPage = () => {
+const StartPage = (props: Props) => {
   return (
     <div className="devtest__first-page">
       <TitleBox
@@ -11,27 +22,51 @@ const StartPage = () => {
         subtitle="과연 나는 어떤 개발자일까? "
       />
       <VisitorBox today={10} sum={100} />
-      <StartButton color="black" title="시작하기" onClickHandler={() => null} />
-      <Button
-        backgroundColor="black"
-        color="white"
-        title="나문희의 첫사랑"
-        onClickHandler={() => null}
+      <StartButton
+        color={COLORS.SSU.DeepBlue}
+        title="시작하기"
+        onClickHandler={() => props.setStage((prev) => prev + 1)}
       />
+      <span>GDSC Soongsil Univ.</span>
       <img src={devTestLogo} alt="devtest-logo" />
       <style jsx>{`
         .devtest__first-page {
           width: 100%;
           height: 100%;
           position: relative;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: flex-start;
+          gap: 30px;
+          padding-top: 80px;
+        }
+        span {
+          color: white;
+          font-size: ${TEXT_STYLE_BODY_PC.body2B.fontSize};
+          font-weight: ${TEXT_STYLE_BODY_PC.body2B.fontWeight};
         }
         img {
-          width: 60%;
+          width: 65%;
           height: auto;
           position: absolute;
           bottom: 0px;
           left: 50%;
           transform: translateX(-50%);
+        }
+
+        @media screen and (max-width: 500px) {
+          span {
+            color: white;
+            font-size: ${TEXT_STYLE_BODY_MOBILE.body2B.fontSize};
+            font-weight: ${TEXT_STYLE_BODY_MOBILE.body2B.fontWeight};
+          }
+        }
+
+        @media screen and (min-height: 800px) {
+          .devtest__first-page {
+            padding-top: 120px;
+          }
         }
       `}</style>
     </div>
--- src/components/comuunity/playground/DevTest/index.tsx
+++ src/components/comuunity/playground/DevTest/index.tsx
@@ -1,14 +1,17 @@
 import Wrapper from '../Wrapper';
 import StartPage from './StartPage';
+import { useState } from 'react';
 
 /**
  * 개발자 성향테스트 페이지 최상위 Wrapper
  */
 const DevTestPage = () => {
+  const [stage, setStage] = useState(0);
+
   return (
     <div className="devtest__page">
       <Wrapper topColor="#00A4CA" bottomColor="#58C4C4">
-        <StartPage />
+        {stage === 0 ? <StartPage setStage={setStage} /> : <></>}
       </Wrapper>
       <style jsx>{`
         .devtest__page {
