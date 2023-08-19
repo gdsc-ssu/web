bf702368e2abbdf7ecf0377ec7263c7c3ac00733
kanghyun98 <lkh981107@gmail.com>
2023-08-19 21:38:36 +0900

    feat: add color contstant file

---
+++ src/constants/styles/color.ts
@@ -0,0 +1,21 @@
+export const COLORS = {
+  primary: {
+    red: '#EA4335',
+    yellow: '#FBBC04',
+    green: '#0F9D58',
+    blue: '#4285F4',
+  },
+  grayscale: {
+    white: '#FFFFFF',
+    gray1: '#F3F5F7',
+    gray2: '#ECEEF0',
+    gray3: '#E2E5E8',
+    gray4: '#D4D8DC',
+    gray5: '#B5B9BD',
+    gray6: '#8E9398',
+    gray7: '#505458',
+    gray8: '#3A3D40',
+    gray9: '#252729',
+    black: '#101112',
+  },
+};
